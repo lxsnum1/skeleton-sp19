ee6e238d434d87c982f0236cd0f35f15568807ee
lixuesong <672528307@qq.com>
2019-11-28 23:19:01 +0800

    basic Boby method  &  code formatted

---
+++ proj0/Body.java
@@ -0,0 +1,71 @@
+/**
+ * Body
+ */
+public class Body {
+
+    public double xxPos; // Its current x positon
+    public double yyPos; // Its current y positon
+    public double xxVel; // Its current velocity in the x direction
+    public double yyVel; // Its current velocity in the y direction
+    public double mass; // Its mass
+    public String imgFileName; // The name of the file that corresponds to the image that depicts the body
+
+    private static final double gravConst = 6.67e-11; // gravitational constant
+
+    public Body(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Body(Body b) {
+        this.xxPos = b.xxPos;
+        this.yyPos = b.yyPos;
+        this.xxVel = b.xxVel;
+        this.yyVel = b.yyVel;
+        this.mass = b.mass;
+        this.imgFileName = b.imgFileName;
+    }
+
+    public double calcDistance(Body b) {
+        double dx = b.xxPos - this.xxPos;
+        double dy = b.yyPos - this.yyPos;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double calcForceExertedBy(Body b) {
+        return gravConst * this.mass * b.mass / (Math.pow(calcDistance(b), 2));
+    }
+
+    public double calcForceExertedByX(Body b) {
+        return calcForceExertedBy(b) * (b.xxPos - xxPos) / calcDistance(b);
+    }
+
+    public double calcForceExertedByY(Body b) {
+        return calcForceExertedBy(b) * (b.yyPos - this.yyPos) / calcDistance(b);
+    }
+
+    public double calcNetForceExertedByX(Body[] b) {
+        double netForceX = 0.0;
+        for (int i = 0; i < b.length; i++) {
+            if (!equals(b[i])) {
+                netForceX += calcForceExertedByX(b[i]);
+            }
+        }
+        return netForceX;
+    }
+
+    public double calcNetForceExertedByY(Body[] b) {
+        double netForceY = 0.0;
+        for (Body someBody : b) {
+            if (!equals(someBody)) {
+                netForceY += calcForceExertedByY(someBody);
+            }
+        }
+        return netForceY;
+    }
+
+}
--- proj0/TestBodyConstructor.java
+++ proj0/TestBodyConstructor.java
@@ -1,63 +1,55 @@
 /**
- *  Tests the Body constructor.
+ * Tests the Body constructor.
  */
 public class TestBodyConstructor {
 
     /**
-     *  Tests the Body constructor to make sure it's working correctly.
+     * Tests the Body constructor to make sure it's working correctly.
      */
     public static void main(String[] args) {
         checkBodyConstructor();
     }
 
     /**
-     *  Checks whether or not two boubles are equal and prints the result.
+     * Checks whether or not two boubles are equal and prints the result.
      *
-     *  @param  expected    Double expected
-     *  @param  actual      Double received
-     *  @param  label       Label for the 'test' case
+     * @param expected Double expected
+     * @param actual   Double received
+     * @param label    Label for the 'test' case
      */
     private static void checkEquals(double expected, double actual, String label) {
         if (Double.isNaN(actual) || Double.isInfinite(actual)) {
-            System.out.println("FAIL: " + label
-                    + ": Expected " + expected + " and you gave " + actual);
+            System.out.println("FAIL: " + label + ": Expected " + expected + " and you gave " + actual);
         } else if (expected == actual) {
-            System.out.println("PASS: " + label
-                    + ": Expected " + expected + " and you gave " + actual);
+            System.out.println("PASS: " + label + ": Expected " + expected + " and you gave " + actual);
         } else {
-            System.out.println("FAIL: " + label
-                    + ": Expected " + expected + " and you gave " + actual);
+            System.out.println("FAIL: " + label + ": Expected " + expected + " and you gave " + actual);
         }
     }
 
     /**
-     *  Checks whether or not two Strings are equal and prints the result.
-     *  @param  expected    String expected
-     *  @param  actual      String received
-     *  @param  label       Label for the 'test' case
+     * Checks whether or not two Strings are equal and prints the result.
+     * 
+     * @param expected String expected
+     * @param actual   String received
+     * @param label    Label for the 'test' case
      */
     private static void checkStringEquals(String expected, String actual, String label) {
         if (expected.equals(actual)) {
-            System.out.println("PASS: " + label
-                    + ": Expected " + expected + " and you gave " + actual);
+            System.out.println("PASS: " + label + ": Expected " + expected + " and you gave " + actual);
         } else {
-            System.out.println("FAIL: " + label
-                    + ": Expected " + expected + " and you gave " + actual);
+            System.out.println("FAIL: " + label + ": Expected " + expected + " and you gave " + actual);
         }
     }
 
     /**
-     *  Checks Body constructors to make sure they are setting instance
-     *  variables correctly.
+     * Checks Body constructors to make sure they are setting instance variables
+     * correctly.
      */
     private static void checkBodyConstructor() {
         System.out.println("Checking first Body constructor...");
 
-        double xxPos = 1.0,
-               yyPos = 2.0,
-               xxVel = 3.0,
-               yyVel = 4.0,
-               mass = 5.0;
+        double xxPos = 1.0, yyPos = 2.0, xxVel = 3.0, yyVel = 4.0, mass = 5.0;
 
         String imgFileName = "jupiter.gif";
 
